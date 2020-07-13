4acfc6dffcc81e1523928786137598e850a993de
Talha Ayub <mtalhaayub752@gmail.com>
2020-07-13 07:34:07 +0000

    aad

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 
-mongoose.connect("mongodb+srv://userdatabase:Yelpcamp123@cluster0.0lolt.mongodb.net/yelppcampp?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://userdatabase:Yelpcamp123@cluster0.0lolt.mongodb.net/yelppcampp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -50,6 +50,13 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(6987, function(){
-   console.log("The YelpCamp Server 6987 has started");
+// app.listen(6987, function(){
+//    console.log("The YelpCamp Server 6987 has started");
+// });
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
