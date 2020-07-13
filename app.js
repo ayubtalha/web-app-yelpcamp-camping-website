cf3d927c7252f25b2f62534db8daea74a5b56c07
Talha Ayub <mtalhaayub752@gmail.com>
2020-07-13 04:58:09 +0000

    added the url to ap js file

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect("mongodb+srv://talhaayub:<Yelpcamp123>@cluster0.0lolt.mongodb.net/<yelp_camp>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
