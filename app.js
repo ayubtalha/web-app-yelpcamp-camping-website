dd2d59edf357767911529b7ed97bbbd784910941
Talha Ayub <mtalhaayub752@gmail.com>
2020-07-13 07:48:05 +0000

    aaad

--- app.js
+++ app.js
@@ -54,9 +54,12 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 //    console.log("The YelpCamp Server 6987 has started");
 // });
 
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
+
+// var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+// var server_host = process.env.YOUR_HOST || '0.0.0.0';
+// server.listen(server_port, server_host, function() {
+//     console.log('Listening on port %d', server_port);
+// });
